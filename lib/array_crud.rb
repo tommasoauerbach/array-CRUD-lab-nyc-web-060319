53c2f4b315aafc6a63b5ed5bd702bcac179bbf92
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 18:35:12 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  artists_hiphop = ["Big", "2pac", "wutang", "nas"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  artists_hiphop = ["Big", "2pac", "wutang", "nas"]
 end
 
+  artists_hiphop << "madvillan"
+end
+
+
 def add_element_to_start_of_array(array, element)
-  
+
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
